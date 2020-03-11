57f0518ce63a9de3cb6a58814080433e8c09818d
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-11 23:49:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-
+def batch_badge_creator(name_array)
+  badge_array = []
+  name_array.each do |name|
+    badge_array << badge_maker(name)
+  end
+  badge_array
 end
