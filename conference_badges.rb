572c3145a109f46a61076e43d70fb9e8c0c18276
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-11 23:54:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,14 +2,18 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name_array)
+def batch_badge_creator(name_list)
   badge_array = []
-  name_array.each do |name|
+  name_list.each do |name|
     badge_array << badge_maker(name)
   end
   badge_array
 end
 
-def assign_rooms
-
+def assign_rooms(name_list)
+  badge_array = []
+  name_list.each_with_index do |name, index|
+    badge_array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  badge_array
 end
